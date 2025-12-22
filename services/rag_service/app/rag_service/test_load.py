e25eed8880d8151c249620573b8077ed0f190a9c
Eli Serov <serov.ilya.2903@yandex.ru>
2025-12-22 19:02:09 +0300

    model test llama.cpp passed/removed test script

--- services/rag_service/app/rag_service/test_load.py
+++
@@ -1,8 +0,0 @@
-import llama_cpp
-
-llm = llama_cpp.Llama(model_path="/app/rag_service/model.gguf", embedding=True)
-
-embeddings = llm.create_embedding("Hello, world!")
-
-print("Эмбеддинг размерности:", len(embeddings))
-print("Пример значений:", embeddings[:10])
